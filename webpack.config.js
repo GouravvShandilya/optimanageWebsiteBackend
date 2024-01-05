e8aff999c3bc75e1fbf1c0b9857549d7e611b16c
Gouravshandilya69 <i.gouravshandilya@gmail.com>
2024-01-05 11:49:29 +0530

    agagaS

--- webpack.config.js
+++
@@ -1,6 +0,0 @@
-const path = require('path');
-
-module.exports = {
-  entry: './app.js', // Example entry point, adjust this path according to your project structure
-  // ... other webpack configurations
-};
